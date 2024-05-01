aba51acccceacc2ca92c34d9627422a20a38c6b5
Nitish <pqnitish2020.com>
2024-05-01 17:32:57 +0530

    added prev-next-btn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 let departSelect = document.getElementById("select-dept");
+let prevBtn = document.getElementById("prevBtn");
+let nextBtn = document.getElementById("nextBtn");
 departSelect.addEventListener("change", function (event) {
   let category = event.target.value;
   filterData(category);
@@ -69,5 +71,37 @@ async function filterBySalary(salary) {
   console.log(finalFilterdData.data);
   showData(finalFilterdData.data);
 }
+let currentPage = 1;
+let itemsPerPage = 10;
+async function getPaginationData(page) {
+  let res =  await fetch(
+    `https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees?page=${page}&limit=${itemsPerPage}`
+  );
+  let finalPaginationData =  await res.json();
+ // console.log(finalPaginationData.data);
+ showData(finalPaginationData.data);
+}
+function next(){
+  currentPage++;
+  if(currentPage==10){
+    nextBtn.disabled = true;
+    prevBtn.disabled = false;
+
+  }
+  getPaginationData(currentPage);
+}
+function prev(){
+  if(currentPage==1){
+    prevBtn.disabled = true;
+  }
+  if(currentPage >1){
+    nextBtn.disabled = false;
+    currentPage--;
+    prevBtn.disabled = false;
+    getPaginationData(currentPage);
+  }
+}
 
 getData();
+getPaginationData(currentPage);
+
